17fd57c4b61048341c3437ecfc53358515c1a040
stealpattan <stealpattan@gmail.com>
2016-12-08 19:51:17 +0900

    test commit

--- read_csv.java
+++ read_csv.java
@@ -1,7 +1,33 @@
-import java.util.*;
+import java.io.*;
 
 public class read_csv{
 	public static void main(String[] arg){
-		System.out.println("hogehoge");
+
+	}
+
+	private void readCsv(String f){
+		int num = 0;
+		try{
+			String data;
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			while((data = br.readLine()) != null){
+				num++;
+			}
+		}catch(IOException e){
+			System.out.println("Cannot open the file");
+		}
+
+		String[] datas = new String[num];
+		num = 0;
+		try{
+			String data;
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			while((data = br.readLine()) != null){
+				datas[num] = data;
+				num++;
+			}
+		}catch(IOException e){
+			System.out.println("Cannot find the file");
+		}
 	}
 }
