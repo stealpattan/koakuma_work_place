68fc59e8e5ce5ee9bb994d000ddb7050a45ed3d2
stealpattan <stealpattan@gmail.com>
2016-12-09 12:47:15 +0900

    とりあえず読み込み成功

--- read_csv.java
+++ read_csv.java
@@ -1,22 +1,28 @@
 import java.io.*;
 
 public class read_csv{
+	static int num = 0;
 	public static void main(String[] arg){
-
+		if(arg != null){
+			int number_of_line = getFileLong("kougakukengai26.csv");
+			System.out.println(number_of_line);
+		}
 	}
 
-	private void readCsv(String f){
-		int num = 0;
+	public static int getFileLong(String f){
 		try{
-			String data;
 			BufferedReader br = new BufferedReader(new FileReader(f));
+			String data;
 			while((data = br.readLine()) != null){
 				num++;
 			}
 		}catch(IOException e){
-			System.out.println("Cannot open the file");
+				System.out.println(e);
 		}
+		return num;
+	}
 
+	private String[] readCsv(String f){
 		String[] datas = new String[num];
 		num = 0;
 		try{
@@ -29,5 +35,6 @@ public class read_csv{
 		}catch(IOException e){
 			System.out.println("Cannot find the file");
 		}
+		return datas;
 	}
 }
