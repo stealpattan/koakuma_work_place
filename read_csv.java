1974c1b927a356a74611adafbaf5280f0f55a120
stealpattan <stealpattan@gmail.com>
2016-12-16 19:06:59 +0900

    csvからの分類分け等に成功

--- read_csv.java
+++ read_csv.java
@@ -8,11 +8,18 @@ public class read_csv{
 		System.out.println(number_of_line);
 		String[][] file_data = new String[number_of_line][27];
 		file_data = readCsv(file_name);
+		//below code is used for test 
 		for (int i=0; i<10; i++) {
-			for(int j=0; j<27; j++){
+			for(int j=0; j<=25; j++){
 				System.out.println("num=" + i + ": kind=" + j + ": data=" + file_data[i][j]);
 			}
 		}
+		for (int i=0; i<=25; i++) {
+			System.out.println(
+				i + ":" + identify(i)+ ":" + file_data[3][i]
+			);
+		}
+		//above code is used for test
 	}
 
 	public static int getFileLong(String f){
@@ -44,12 +51,52 @@ public class read_csv{
 				for(int j=0;j<split_data.length;j++){
 					datas[num][j] = split_data[j];
 				}
-//				datas[num] = data;
 				num++;
 			}
 		}catch(IOException e){
 			System.out.println("Cannot find the file");
 		}
 		return datas;
+	}
+
+	public static String identify(int num){
+		String st = "no_data";
+		int extra_num;
+		if(num <= 6){
+			switch(num){
+				case 0:
+					st = "企業名";
+					break;
+				case 1:
+					st = "職種";
+					break;
+				case 2:
+					st = "機械";
+					break;
+				case 3:
+					st = "知能";
+					break;
+				case 4:
+					st = "情報";
+					break;
+				case 5:
+					st = "生物";
+					break;
+				case 6:
+					st = "環境";
+					break;
+				default:
+					st = "エラー";
+					break;
+			}
+		}
+		else if(num > 6){
+			extra_num = num % 7 + 2;
+			st = identify(extra_num);
+		}
+		else{
+			System.out.println("Something error is happened");
+		}
+		return st;
 	}
 }
